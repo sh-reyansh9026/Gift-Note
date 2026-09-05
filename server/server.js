6a5c35a41e062333c30e6f6bf587698e02011c76
sh-reyansh9026 <sh.reyansh9026@gmail.com>
2026-09-05 12:07:48 +0530

    Fix

--- server/server.js
+++ server/server.js
@@ -81,25 +81,6 @@ app.get("/api/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-app.get("/", (req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta name="google-site-verification" content="l8hBNbD330EemDIerJFTYy89mV21qfzOA1nHLRrCiDM" />
-        <title>GiftNote API</title>
-      </head>
-      <body>
-        GiftNote API
-      </body>
-    </html>
-  `);
-});
-
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
